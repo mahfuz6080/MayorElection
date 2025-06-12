656621116a80b1aa9b9233ff2165d280dd7ed43e
Md Mahfuz Rahman <tanzimrani8@gmail.com>
2025-06-12 11:40:13 +0530

    Update ElectionManager.java

--- src/main/java/me/arcx/mayor/ElectionManager.java
+++ src/main/java/me/arcx/mayor/ElectionManager.java
@@ -1,49 +1,29 @@
 package me.arcx.mayor.election;
 
-import me.arcx.mayor.npc.MayorNPC;
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.configuration.file.FileConfiguration;
+import me.arcx.mayor.gui.MayorGUI;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class ElectionManager {
 
-    private final JavaPlugin plugin;
-    private final List<String> allMayors;
+    private final List<String> allMayors = List.of(
+        "Diana", "Cole", "Paul", "Derpy", "Marina",
+        "Jerry", "Aatrox", "Diaz", "Foxy", "Finnegan",
+        "Technoblade", "Barry"
+    );
 
-    public ElectionManager(JavaPlugin plugin) {
-        this.plugin = plugin;
-        FileConfiguration config = plugin.getConfig();
-        this.allMayors = config.getStringList("mayors");
+    public void openMayorGUI(Player player) {
+        List<String> selectedMayors = getRandomMayors(5);
+        MayorGUI gui = new MayorGUI(selectedMayors);
+        gui.open(player);
     }
 
-    public List<String> getRandomMayors(int count) {
+    private List<String> getRandomMayors(int count) {
         List<String> copy = new ArrayList<>(allMayors);
         Collections.shuffle(copy);
         return copy.subList(0, Math.min(count, copy.size()));
-    }
-
-    public void spawnMayors(Player viewer) {
-        List<String> selectedMayors = getRandomMayors(5);
-
-        List<Location> locations = Arrays.asList(
-                new Location(Bukkit.getWorld("world"), -36.5, 71, -44.5),
-                new Location(Bukkit.getWorld("world"), -34.5, 71, -44.5),
-                new Location(Bukkit.getWorld("world"), -32.5, 71, -44.5),
-                new Location(Bukkit.getWorld("world"), -30.5, 71, -44.5),
-                new Location(Bukkit.getWorld("world"), -28.5, 71, -44.5)
-        );
-
-        for (int i = 0; i < selectedMayors.size(); i++) {
-            String name = selectedMayors.get(i);
-            Location loc = locations.get(i);
-            MayorNPC npc = new MayorNPC(name, loc.getX(), loc.getY(), loc.getZ(), 0f, 0f);
-            npc.spawn(viewer);
-        }
-
-        viewer.sendMessage("Spawned 5 random mayors.");
     }
 }
