fd7bc56f478a355c81d54b107c37e23d06877c7c
Md Mahfuz Rahman <tanzimrani8@gmail.com>
2025-06-12 11:16:49 +0530

    Update MayorElection.java

--- src/main/java/me/arcx/mayor/MayorElection.java
+++ src/main/java/me/arcx/mayor/MayorElection.java
@@ -1,51 +1,25 @@
 package me.arcx.mayor;
 
-import org.bukkit.Bukkit;
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.plugin.java.JavaPlugin;
+import net.citizensnpcs.api.CitizensAPI; import net.citizensnpcs.api.npc.NPC; import net.citizensnpcs.api.trait.Trait; import org.bukkit.Bukkit; import org.bukkit.ChatColor; import org.bukkit.Location; import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.ArrayList;
-import java.util.List;
+public class MayorElection extends JavaPlugin { @Override public void onEnable() { getLogger().info("Mayor Election plugin has been enabled.");
 
-public class MayorElection extends JavaPlugin implements Listener {
+// Create NPC
+    Bukkit.getScheduler().runTaskLater(this, () -> {
+        NPC npc = CitizensAPI.getNPCRegistry().createNPC(
+                org.bukkit.entity.EntityType.PLAYER, "MayorCandidate");
 
-    private ElectionManager manager;
-    private MayorGUI gui;
-
-    @Override
-    public void onEnable() {
-        saveDefaultConfig();
-        manager = new ElectionManager();
-        gui = new MayorGUI(manager);
-        Bukkit.getPluginManager().registerEvents(this, this);
-        getCommand("election").setExecutor((sender, cmd, label, args) -> {
-            if (!(sender instanceof org.bukkit.entity.Player p)) return false;
-            gui.open(p);
-            return true;
-        });
-
-        List<Mayor> allMayors = loadMayors();
-        manager.startElection(allMayors);
-    }
-
-    @EventHandler
-    public void onInvClick(InventoryClickEvent e) {
-        if (e.getWhoClicked() instanceof org.bukkit.entity.Player p) {
-            gui.onClick(p, e);
-        }
-    }
-
-    private List<Mayor> loadMayors() {
-        FileConfiguration cfg = getConfig();
-        List<Mayor> list = new ArrayList<>();
-        for (String key : cfg.getConfigurationSection("mayors").getKeys(false)) {
-            String skin = cfg.getString("mayors." + key + ".skin");
-            List<String> perks = cfg.getStringList("mayors." + key + ".perks");
-            list.add(new Mayor(key, skin, perks));
-        }
-        return list;
-    }
+        Location spawnLocation = new Location(Bukkit.getWorld("world"), 5, 70, -3);
+        npc.spawn(spawnLocation);
+        npc.getEntity().setCustomName(ChatColor.YELLOW + "Candidate: " + ChatColor.GREEN + "Diana");
+        npc.getEntity().setCustomNameVisible(true);
+    }, 20L);
 }
+
+@Override
+public void onDisable() {
+    getLogger().info("Mayor Election plugin has been disabled.");
+}
+
+}
+
