a4c01c805e275fe2a0b99bc4bd7a53a92037ca78
Md Mahfuz Rahman <tanzimrani8@gmail.com>
2025-06-12 11:35:21 +0530

    Update MayorElection.java

--- src/main/java/me/arcx/mayor/MayorElection.java
+++ src/main/java/me/arcx/mayor/MayorElection.java
@@ -1,31 +1,33 @@
-// File: MayorElection.java
 package me.arcx.mayor;
 
-import me.arcx.mayor.npc.MayorNPC;
+import me.arcx.mayor.election.ElectionManager;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MayorElection extends JavaPlugin {
 
+    private ElectionManager electionManager;
+
     @Override
     public void onEnable() {
-        getLogger().info("[MayorElection] Plugin enabled");
+        saveDefaultConfig();
+        this.electionManager = new ElectionManager(this);
 
-        // Register command for testing
-        getCommand("spawnmayor").setExecutor((sender, command, label, args) -> {
-            if (!(sender instanceof Player)) return false;
-            Player player = (Player) sender;
-
-            MayorNPC npc = new MayorNPC("Mayor_" + player.getName(), player.getLocation().getX() + 1, player.getLocation().getY(), player.getLocation().getZ(), 0f, 0f);
-            npc.spawn(player);
-            player.sendMessage("Spawned test mayor NPC!");
-            return true;
+        getCommand("spawnmayors").setExecutor((sender, command, label, args) -> {
+            if (sender instanceof Player) {
+                electionManager.spawnMayors((Player) sender);
+                return true;
+            }
+            sender.sendMessage("This command is for players only.");
+            return false;
         });
+
+        getLogger().info("[MayorElection] Enabled successfully.");
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("[MayorElection] Plugin disabled");
+        getLogger().info("[MayorElection] Disabled.");
     }
 }
