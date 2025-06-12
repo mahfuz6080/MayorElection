9cab36afcb4f90bf6ce47f1cc722df0c63ec5a8f
Md Mahfuz Rahman <tanzimrani8@gmail.com>
2025-06-12 11:29:56 +0530

    Update MayorElection.java

--- src/main/java/me/arcx/mayor/MayorElection.java
+++ src/main/java/me/arcx/mayor/MayorElection.java
@@ -1,25 +1,31 @@
+// File: MayorElection.java
 package me.arcx.mayor;
 
-import net.citizensnpcs.api.CitizensAPI; import net.citizensnpcs.api.npc.NPC; import net.citizensnpcs.api.trait.Trait; import org.bukkit.Bukkit; import org.bukkit.ChatColor; import org.bukkit.Location; import org.bukkit.plugin.java.JavaPlugin;
+import me.arcx.mayor.npc.MayorNPC;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
 
-public class MayorElection extends JavaPlugin { @Override public void onEnable() { getLogger().info("Mayor Election plugin has been enabled.");
+public class MayorElection extends JavaPlugin {
 
-// Create NPC
-    Bukkit.getScheduler().runTaskLater(this, () -> {
-        NPC npc = CitizensAPI.getNPCRegistry().createNPC(
-                org.bukkit.entity.EntityType.PLAYER, "MayorCandidate");
+    @Override
+    public void onEnable() {
+        getLogger().info("[MayorElection] Plugin enabled");
 
-        Location spawnLocation = new Location(Bukkit.getWorld("world"), 5, 70, -3);
-        npc.spawn(spawnLocation);
-        npc.getEntity().setCustomName(ChatColor.YELLOW + "Candidate: " + ChatColor.GREEN + "Diana");
-        npc.getEntity().setCustomNameVisible(true);
-    }, 20L);
+        // Register command for testing
+        getCommand("spawnmayor").setExecutor((sender, command, label, args) -> {
+            if (!(sender instanceof Player)) return false;
+            Player player = (Player) sender;
+
+            MayorNPC npc = new MayorNPC("Mayor_" + player.getName(), player.getLocation().getX() + 1, player.getLocation().getY(), player.getLocation().getZ(), 0f, 0f);
+            npc.spawn(player);
+            player.sendMessage("Spawned test mayor NPC!");
+            return true;
+        });
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("[MayorElection] Plugin disabled");
+    }
 }
-
-@Override
-public void onDisable() {
-    getLogger().info("Mayor Election plugin has been disabled.");
-}
-
-}
-
